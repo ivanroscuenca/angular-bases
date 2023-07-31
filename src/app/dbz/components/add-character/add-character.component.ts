7bfc881bfd1fd443f5a979fdb8d81d5c1f781310
ivanroscuenca <ivanros@protonmail.com>
2023-07-31 17:35:33 +0200

    video59

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,21 +1,8 @@
 import { Component } from '@angular/core';
-import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css'],
 })
-export class AddCharacterComponent {
-  public character: Character = {
-    name: '',
-    power: 0,
-  };
-
-  emitCharacter(): void {
-    console.log(this.character);
-
-    this.character.name = '';
-    this.character.power = 0;
-  }
-}
+export class AddCharacterComponent {}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   exports: [MainPageComponent],
 })
 export class DbzModule {}
