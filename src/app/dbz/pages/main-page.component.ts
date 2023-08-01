48d060d7c49f39fca87cf7630d7e28e385107712
ivanroscuenca <ivanros@protonmail.com>
2023-08-01 23:18:06 +0200

    video70Seccion5Acabada

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbzService } from '../dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,5 +8,17 @@ import { DbzService } from '../dbz.service';
 })
 export class MainPageComponent {
   //inyectamos servicio en en el constructor
-  constructor(public dbzService: DbzService) {}
+  constructor(private dbzService: DbzService) {}
+  //getters
+  get characters(): Character[] {
+    return this.dbzService.characters;
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character) {
+    this.dbzService.addNewCharacter(character);
+  }
 }
