d27170e955195111946fab46d73211899fbaa97f
ivanroscuenca <ivanros@protonmail.com>
2023-08-01 17:43:55 +0200

    video 60 modificado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 10000,
+    },
+    {
+      name: 'Vegeta',
+      power: 8000,
+    },
+  ];
+}
