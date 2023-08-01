20e8a0972d8f4296e548973b51966233a682eef5
ivanroscuenca <ivanros@protonmail.com>
2023-08-01 17:43:43 +0200

    video60 modificado

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,17 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { Character } from '../../interfaces/character.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {
-  @Input()
-  public characterList: Character[] = [
-    {
-      name: 'trunks',
-      power: 10,
-    },
-  ];
-}
+export class ListComponent {}
